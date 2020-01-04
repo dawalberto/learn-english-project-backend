db15e8be8567624eaa16224655f38252215b9d28
XavCharls <sujetonumero5@gmail.com>
2020-01-04 13:53:03 +0100

    funcitons text and order

--- functions.js
+++ functions.js
@@ -1,4 +1,20 @@
 
 function textToArray(text){
+    let sanityzedText = text //albertFunction
+    let result = []
+  
+    array_words = sanityzedText.split(" ");
+    array_words.sort()
 
+    array_words.forEach((word) => {
+        if(!result.find(element => (element[word])?element[word]++:false)){
+            result.push(JSON.parse('{"'+word+'":1}'))
+        }
+    })
+
+    return result
+}
+
+function orderByMax(arrayJsons){
+    return arrayJsons.sort((a, b) => parseFloat(JSON.stringify(b).split(":")[1]) - parseFloat(JSON.stringify(a).split(":")[1]))
 }
